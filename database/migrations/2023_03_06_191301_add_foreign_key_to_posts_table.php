30f3249ae7f0ffac784c6a5ce3682d91bef82537
robertvlad <vladrob96@gmail.com>
2023-03-06 20:03:11 +0100

    creata colonna della foreign key type_id

---
+++ database/migrations/2023_03_06_191301_add_foreign_key_to_posts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+
+            //CREARE LA COLONNA DELLA FOREIGN KEY
+            $table->unsignedBigInteger('type_id')
+                ->nullable()
+                ->after('id');
+            
+            //CREARE LA FOREIGN KEY
+            $table->foreign('type_id')
+                ->references('id')
+                ->on('types')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+
+            $table->dropForeign('posts_type_id_foreign');
+
+            $table->dropColumn('type_id');
+        });
+    }
+};
